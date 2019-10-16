8da5c2d58fdbd5fe0530887947d14bf470a411d2
刘祥 <xiang.liu@ymm56.com>
2019-10-16 10:59:25 +0800

    add main

--- utubevideo_recsys_main.py
+++ utubevideo_recsys_main.py
@@ -2,3 +2,10 @@
 # @Time    : 2017/1/10 9:12
 # @Author  : RIO
 # @desc: 工程入口
+from recall.model import run_graph as recall_model
+from rank.model import run_graph as rank_model
+
+# 召回
+recall_model()
+# 排序
+rank_model()
